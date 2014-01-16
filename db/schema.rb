7de78a55f35df0e8d0b6fb1d21d35db7f09a6afe
Albert Agram <albert.agram@gmail.com>
2014-01-16 16:14:24 +0000

    Shop class has_many implementation code

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140116155607) do
+ActiveRecord::Schema.define(version: 20140116160717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20140116155607) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "shop_id"
+  end
+
+  create_table "shops", force: true do |t|
+    t.string   "name"
+    t.string   "location"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
