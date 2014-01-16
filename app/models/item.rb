adb3cd929e1a59f1e0001b5804058c0acf356d67
Albert Agram <albert.agram@gmail.com>
2014-01-16 16:02:28 +0000

    simple validations for presence_of

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ActiveRecord::Base
   belongs_to :shop
+
+  validates_presence_of :name, :price
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,4 +2,6 @@ require 'spec_helper'
 
 describe Item do
  it { should belong_to(:shop) }
+ it { should validate_presence_of(:name) }
+ it { should validate_presence_of(:price) }
 end
