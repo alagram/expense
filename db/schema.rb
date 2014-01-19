8807c6126b5cfc8860c50c78f55f201dc7294815
Albert Agram <albert.agram@gmail.com>
2014-01-19 20:43:14 +0000

    removed location from shops table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140116160717) do
+ActiveRecord::Schema.define(version: 20140119181730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 20140116160717) do
 
   create_table "shops", force: true do |t|
     t.string   "name"
-    t.string   "location"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
