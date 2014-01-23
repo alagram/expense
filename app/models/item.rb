a3fe29331aab70e873ae5b7031578827ed320b13
Albert Agram <albert.agram@gmail.com>
2014-01-23 10:44:53 +0000

    new feature implementation

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,11 @@ class Item < ActiveRecord::Base
   validates_presence_of :name, :price
 
   attr_accessor :new_shop
+  before_save :create_shop
+
+  private
+
+  def create_shop
+    self.shop = Shop.create!(name: new_shop) if new_shop.present?
+  end
 end
