0c2fad2bcf0685574eab0d6486fd15920d9f0852
Albert Agram <albert.agram@gmail.com>
2014-01-27 14:14:53 +0000

    validate numericality of price

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,4 +4,5 @@ describe Item do
  it { should belong_to(:shop) }
  it { should validate_presence_of(:name) }
  it { should validate_presence_of(:price) }
+ it { should validate_numericality_of(:price) }
 end
