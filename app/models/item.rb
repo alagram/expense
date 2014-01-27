5be8e93008fc4ab3e2fb42647f2e5834837a4315
Albert Agram <albert.agram@gmail.com>
2014-01-27 14:15:44 +0000

    price validation implementation

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ActiveRecord::Base
   belongs_to :shop
 
   validates_presence_of :name, :price
+  validates :price, numericality: true, format: { with: /^\d{1,6}(\.\d{0,2})?$/, multiline: true }
 
   attr_accessor :new_shop
   before_save :create_shop
