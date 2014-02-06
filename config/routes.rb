d8054555e90912427d035d2b9e99c98aa044d9af
Albert Agram <albert.agram@gmail.com>
2014-02-06 14:23:17 +0000

    define search route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Expense::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
   root to: 'items#index'
 
-  resources :items, only: [:new, :create]
+  resources :items, only: [:new, :create] do
+    collection do
+      get 'search', to: 'items#search'
+    end
+  end
 
   get 'find', to: 'items#find'
 end
