806b11ff82b9ab695b68768dc84024666abcefa9
Albert Agram <albert.agram@gmail.com>
2014-02-06 18:01:01 +0000

    first spec for search for exact match

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,4 +5,11 @@ describe Item do
  it { should validate_presence_of(:name) }
  it { should validate_presence_of(:price) }
  it { should validate_numericality_of(:price) }
+
+ describe "search" do
+  it "returns an array of one item for an exact match" do
+    item = Fabricate(:item)
+    expect(Item.search("2014-02-01", "2014-02-06")).to eq([item])
+  end
+ end
 end
