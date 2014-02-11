2ee0cdc1586a54cf9bebf1b8622b982af6d93c21
Albert Agram <albert.agram@gmail.com>
2014-02-11 19:06:56 +0000

    testing new edge cases in items controller

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -10,7 +10,7 @@ describe ItemsController do
   end
 
   describe "GET index" do
-    it "sets the @items instance varaible" do
+    it "sets the @items instance variable" do
       shop = Fabricate(:shop)
       egg = Fabricate(:item, shop_id: shop.id)
       milk = Fabricate(:item, shop_id: shop.id)
@@ -62,13 +62,39 @@ describe ItemsController do
 
   describe "GET search" do
     context "with valid attributes" do
-      it "sets @results instance" do
+      it "sets @results instance variable" do
         item1 = Fabricate(:item, created_at: 1.month.ago)
         item2 = Fabricate(:item)
         get :search, s: "2014-01-01", e: "#{Date.today}"
         expect(assigns(:results)).to match_array([item2, item1])
       end
     end
-    context "with invalid attributes"
+
+    context "with invalid attributes" do
+      it "does not set @ results instance variable" do
+        item1 = Fabricate(:item, created_at: 1.month.ago)
+        item2 = Fabricate(:item)
+        get :search, s: "", e: ""
+        expect(assigns(:results)).to be_nil
+      end
+      it "renders find page with empty params" do
+        item1 = Fabricate(:item, created_at: 1.month.ago)
+        item2 = Fabricate(:item)
+        get :search, s: "", e: nil
+        expect(response).to render_template :find
+      end
+      it "renders the find page with an invalid date" do
+        item1 = Fabricate(:item, created_at: 1.month.ago)
+        item2 = Fabricate(:item)
+        get :search, s: "2014-01-", e: "#{Date.today}"
+        expect(response).to render_template :find
+      end
+      it "sets flash notice" do
+        item1 = Fabricate(:item, created_at: 1.month.ago)
+        item2 = Fabricate(:item)
+        get :search, s: "", e: ""
+        expect(flash[:error]).to be_present
+      end
+    end
   end
 end
