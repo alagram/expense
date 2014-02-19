a81f02beb1c7204c8260dbff4aa4c5e3831415e7
Albert Agram <albert.agram@gmail.com>
2014-02-19 18:40:24 +0000

    implementation to make specs pass

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,10 +19,38 @@ class ItemsController < ApplicationController
   end
 
   def search
-    @results = Item.includes(:shop).search(params[:s], params[:e])
+    if (both_params_blank?) || (end_param_blank?) || (start_param_blank?)
+      flash.now[:error] = "Your input is incorrect."
+      render :find
+    else
+      params_valid?
+    end
   end
 
   private
+
+  def both_params_blank?
+    params[:s].blank? && params[:e].blank?
+  end
+
+  def end_param_blank?
+    params[:e].blank?
+  end
+
+  def start_param_blank?
+    params[:s].blank?
+  end
+
+  def params_valid?
+    begin
+      s = Date.parse(params[:s])
+      e = Date.parse(params[:e])
+      @results = Item.includes(:shop).search(s, e)
+    rescue ArgumentError
+      flash[:danger] = "Invalid date."
+      redirect_to find_path
+    end
+  end
 
   def item_params
     params.require(:item).permit(:name, :price, :shop_id, :new_shop, :description)
