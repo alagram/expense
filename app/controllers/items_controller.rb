4792c122e81e5b5bff4f5c7d7b58c528d0323174
Albert Agram <albert.agram@gmail.com>
2014-02-20 08:22:16 +0000

    refactor items controller implementation code
    - remove begin .. end since we're in a method.
    - merged params_valid? method into search action.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,38 +19,13 @@ class ItemsController < ApplicationController
   end
 
   def search
-    if (both_params_blank?) || (end_param_blank?) || (start_param_blank?)
-      flash.now[:error] = "Your input is incorrect."
-      render :find
-    else
-      params_valid?
-    end
+    @results = Item.includes(:shop).search(Date.parse(params[:s]), Date.parse(params[:e]))
+  rescue ArgumentError
+    flash[:error] = "Invalid date. Please check your input."
+    redirect_to find_path
   end
 
   private
-
-  def both_params_blank?
-    params[:s].blank? && params[:e].blank?
-  end
-
-  def end_param_blank?
-    params[:e].blank?
-  end
-
-  def start_param_blank?
-    params[:s].blank?
-  end
-
-  def params_valid?
-    begin
-      s = Date.parse(params[:s])
-      e = Date.parse(params[:e])
-      @results = Item.includes(:shop).search(s, e)
-    rescue ArgumentError
-      flash[:danger] = "Invalid date."
-      redirect_to find_path
-    end
-  end
 
   def item_params
     params.require(:item).permit(:name, :price, :shop_id, :new_shop, :description)
