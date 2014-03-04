27b21a80072c037615039120b96de4fbb04b4af5
Albert Agram <albert.agram@gmail.com>
2014-03-04 21:39:02 +0000

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140304190549) do
+ActiveRecord::Schema.define(version: 20140304213614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,9 +33,9 @@ ActiveRecord::Schema.define(version: 20140304190549) do
 
   create_table "users", force: true do |t|
     t.string   "username"
-    t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
