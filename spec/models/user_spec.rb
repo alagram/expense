b76c82a08ecda72f6358495b1b0729a639a82e61
Albert Agram <albert.agram@gmail.com>
2014-03-05 16:49:45 +0000

    test validations for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,9 @@ require 'spec_helper'
 describe User do
   it { should have_many(:items) }
   it { should have_secure_password }
+  it { should validate_presence_of(:username) }
+  it { should validate_uniqueness_of(:username) }
+  it { should validate_presence_of(:password).on(:create) }
+  it { should ensure_length_of(:username).is_at_least(5).is_at_most(20) }
+  it { should ensure_length_of(:password).is_at_least(5).is_at_most(20) }
 end
