2f2547da30cc25f4d44b4a01df3cf259b05ce964
Albert Agram <albert.agram@gmail.com>
2014-03-05 12:10:38 +0000

    User controller create action happy path

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -10,5 +10,26 @@ describe UsersController do
   end
 
   describe "POST create" do
+    context "with valid attributes" do
+      it "redirects to sign in page" do
+        post :create, user: Fabricate.attributes_for(:user)
+        expect(response).to redirect_to sign_in_path
+      end
+
+      it "creates a new user" do
+        post :create, user: Fabricate.attributes_for(:user)
+        expect(User.count).to eq(1)
+      end
+      it "sets flash success mesaage" do
+        post :create, user: Fabricate.attributes_for(:user)
+        expect(flash[:success]).to be_present
+      end
+    end
+
+    context "with invalid attributes" do
+      it "renders new page"
+      it "sets flash danger message"
+      it "sets @user instance variable"
+    end
   end
 end
