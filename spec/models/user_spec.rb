b91ba947058d79196367abc81acaf2601023544e
Albert Agram <albert.agram@gmail.com>
2014-03-05 18:48:17 +0000

    modify spec for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,6 @@ describe User do
   it { should validate_presence_of(:username) }
   it { should validate_uniqueness_of(:username) }
   it { should validate_presence_of(:password).on(:create) }
-  it { should ensure_length_of(:username).is_at_least(5).is_at_most(20) }
+  it { should ensure_length_of(:username).is_at_least(3).is_at_most(20) }
   it { should ensure_length_of(:password).is_at_least(5).is_at_most(20) }
 end
