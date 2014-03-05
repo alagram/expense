d4f9e2abfe6a5c3c3d9daecde62bfee0f5a3dc59
Albert Agram <albert.agram@gmail.com>
2014-03-05 12:11:57 +0000

    Implementation to make users controller happy path spec pass

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,18 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to sign_in_path
+      flash[:success] = "Registration successful. Please sign in!"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 end
