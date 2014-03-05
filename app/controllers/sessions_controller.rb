a6b1485aca951161ec7d3b19221d4a2ee0ee0456
Albert Agram <albert.agram@gmail.com>
2014-03-05 18:05:42 +0000

    implementation code to make all sessions controller create action specs pass

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,9 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to home_path, notice: "Signed in successfully."
+    else
+      redirect_to sign_in_path
+      flash[:danger] = "Invalid username or password."
     end
   end
 end
