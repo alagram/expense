1b9c162ed3058bde18ec77295f676834fe924e0d
Albert Agram <albert.agram@gmail.com>
2014-03-05 17:49:52 +0000

    sessions controller spec happy path

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,6 +1,25 @@
 require 'spec_helper'
 
 describe SessionsController do
-  context "with valid attributes"
-  context "with invalid attributes"
+  describe "POST create" do
+    context "with valid credentials" do
+      it "redirects to home page" do
+        bob = Fabricate(:user)
+        post :create, username: bob.username, password: bob.password
+        expect(response).to redirect_to home_path
+      end
+      it "sets the user into the session" do
+        bob = Fabricate(:user)
+        post :create, username: bob.username, password: bob.password
+        expect(session[:user_id]).to eq(bob.id)
+      end
+      it "sets flash notice message" do
+        bob = Fabricate(:user)
+        post :create, username: bob.username, password: bob.password
+        expect(flash[:notice]).to be_present
+      end
+    end
+
+    context "with invalid credentials"
+  end
 end
