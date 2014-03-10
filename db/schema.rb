9a9a8297b921b18b34a74058a7d8a6edf7c0f024
Albert Agram <albert.agram@gmail.com>
2014-03-10 19:55:41 +0000

    added quantity column to items table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140304214324) do
+ActiveRecord::Schema.define(version: 20140310194852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20140304214324) do
     t.integer  "shop_id"
     t.string   "description"
     t.integer  "user_id"
+    t.string   "quantity"
   end
 
   create_table "shops", force: true do |t|
