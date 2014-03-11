808d3617b4dd4255a0207359a073e1490ecd86c5
Albert Agram <albert.agram@gmail.com>
2014-03-11 09:50:15 +0000

    make valuation for quantity pass

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,7 @@ class Item < ActiveRecord::Base
 
   validates_presence_of :name, :price
   validates :price, numericality: true, format: { with: /^\d{1,6}(\.\d{0,2})?$/, multiline: true }
+  validates_numericality_of :quantity, greater_than: 0
 
   attr_accessor :new_shop
   before_save :create_shop
