b3f4686b0efcbae54993b91f858efcbdd02109c3
Albert Agram <albert.agram@gmail.com>
2014-03-12 08:00:44 +0000

    spec: validate presence of quantity

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,6 +4,7 @@ describe Item do
  it { should belong_to(:shop) }
  it { should validate_presence_of(:name) }
  it { should validate_presence_of(:price) }
+ it { should validate_presence_of(:quantity) }
  it { should validate_numericality_of(:price) }
  it { should belong_to(:user) }
  it { should validate_numericality_of(:quantity).is_greater_than(0) }
