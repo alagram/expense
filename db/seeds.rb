ecf3c46ea9b79807424ad96de0b098fe639f26ff
Albert Agram <albert.agram@gmail.com>
2014-03-14 17:31:21 +0000

    modify seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ game = Shop.create(name: "Game")
 salinesta = Shop.create(name: "Salinesta")
 
 Shop.all.each do |shop|
-  3.times { Fabricate(:item, shop_id: shop.id, user: alice) }
-  2.times { Fabricate(:item, shop_id: shop.id, user: joe) }
-  4.times { Fabricate(:item, shop_id: shop.id, user: john) }
+  3.times { Fabricate(:item, user: alice, shop: shoprite) }
+  2.times { Fabricate(:item, user: joe, shop: marina) }
+  4.times { Fabricate(:item, user: john, shop: game) }
 end
