c04933ee00bfc3a81d66babea35c11b429cbd9d5
Albert Agram <albert.agram@gmail.com>
2014-03-14 12:58:11 +0000

    add bought_at attribute to items fabricator

--- spec/fabricators/item_fabricator.rb
+++ spec/fabricators/item_fabricator.rb
@@ -2,5 +2,6 @@ Fabricator(:item) do
   name { Faker::Lorem.word.capitalize }
   price { rand(1..200) }
   quantity { (1..5).to_a.sample }
+  bought_at { rand(3.years).ago }
   description { Faker::Lorem.words(3).join(" ") }
 end
