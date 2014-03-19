1c8927b9d79635a527b6ff0f47e4240309e139d1
Albert Agram <albert.agram@gmail.com>
2014-03-19 10:27:00 +0000

    implementation code to make all items controller specs pass

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,8 @@ class Item < ActiveRecord::Base
   belongs_to :shop
   belongs_to :user
 
-  validates_presence_of :name, :price, :quantity, :shop
+  validates_presence_of :name, :price, :quantity
+  validates_presence_of :shop_id, if: -> { new_shop_blank? }
   validates :price, numericality: true, format: { with: /^\d{1,6}(\.\d{0,2})?$/, multiline: true }
   validates_numericality_of :quantity, greater_than: 0, only_integer: true
   validate :purchased_at_cannot_be_in_the_future
@@ -29,6 +30,15 @@ class Item < ActiveRecord::Base
   private
 
   def create_shop
-    self.shop = Shop.create!(name: new_shop) if new_shop.present?
+    if new_shop.present? && shop_id.nil?
+      self.shop = Shop.create!(name: new_shop)
+    elsif new_shop.present? && shop_id.present?
+      errors[:base] << "Either enter shop name or select shop."
+      false
+    end
+  end
+
+  def new_shop_blank?
+    new_shop.blank?
   end
 end
