86e0426ebf534456b359d7d3b09d6c0b0a54955b
Albert Agram <albert.agram@gmail.com>
2014-03-19 10:36:46 +0000

    make all item specs pass after changes to item fabricator

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,19 +11,22 @@ describe Item do
  it { should validate_numericality_of(:quantity).only_integer }
 
  describe "search" do
+
+  let!(:shop) { Fabricate(:shop) }
+
   it "returns an array of one item for an exact match" do
-    item = Fabricate(:item)
+    item = Fabricate(:item, shop: shop)
     expect(Item.search("2014-02-01", Time.now)).to eq([item])
   end
   it "returns an array of all matches ordered by created at descending" do
-    item1 = Fabricate(:item, created_at: 1.month.ago)
-    item2 = Fabricate(:item, created_at: 2.weeks.ago)
-    item3 = Fabricate(:item)
+    item1 = Fabricate(:item, created_at: 1.month.ago, shop: shop)
+    item2 = Fabricate(:item, created_at: 2.weeks.ago, shop: shop)
+    item3 = Fabricate(:item, shop: shop)
     expect(Item.search("2013-12-01", Time.now)).to eq([item3, item2, item1])
   end
   it "returns an empty array for a search that matches nothing" do
-    item1 = Fabricate(:item, created_at: 1.month.ago)
-    item2 = Fabricate(:item)
+    item1 = Fabricate(:item, created_at: 1.month.ago, shop: shop)
+    item2 = Fabricate(:item, shop: shop)
     expect(Item.search("2012-12-01", "2013-02-06")).to eq([])
   end
  end
