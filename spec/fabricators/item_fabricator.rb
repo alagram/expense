b53c70be4403f85f83c2dc5f747ef9f74dec549e
Albert Agram <albert.agram@gmail.com>
2014-03-21 11:32:09 +0000

    update item fabricator

--- spec/fabricators/item_fabricator.rb
+++ spec/fabricators/item_fabricator.rb
@@ -2,6 +2,6 @@ Fabricator(:item) do
   name { Faker::Commerce.product_name }
   price { rand(1..200) }
   quantity { (1..5).to_a.sample }
-  purchased_at { rand(3.years).ago }
+  purchased_at { rand(1..30).days.ago }
   description { Faker::Lorem.words(3).join(" ") }
 end
