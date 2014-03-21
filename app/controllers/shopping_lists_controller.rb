1da26392be5ef10740129e5790bd1da818dce35b
Albert Agram <albert.agram@gmail.com>
2014-03-21 13:28:10 +0000

    Implementation code to make ShoppingList controller happy path pass

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -4,5 +4,20 @@ class ShoppingListsController < ApplicationController
   end
 
   def create
+    @shopping_list = ShoppingList.new(shopping_list_params.merge(user: current_user))
+
+    if @shopping_list.save
+      redirect_to new_shopping_list_path
+      flash[:success] = "You successfully created a Shopping list."
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def shopping_list_params
+    params.require(:shopping_list).permit(:name, :description)
   end
 end
