97a034fbec5c78ba8be49abcf0582227792421e0
Albert Agram <albert.agram@gmail.com>
2014-03-23 22:26:07 +0000

    ListItems controller specs: sad path

--- spec/controllers/list_items_controller.rb
+++ spec/controllers/list_items_controller.rb
@@ -6,11 +6,11 @@ describe ListItemsController do
 
       context "with valid attributes" do
 
-        it "redirects to list_list_items page" do
+        it "redirects to list show page" do
           set_current_user
           list = Fabricate(:list)
           post :create, list_item: Fabricate.attributes_for(:list_item, name: "My List"), list_id: list.id
-          expect(response).to redirect_to list_list_items_path
+          expect(response).to redirect_to list
         end
         it "create a new list item" do
           set_current_user
@@ -33,9 +33,42 @@ describe ListItemsController do
         end
       end
 
-      context "with invalid attributes"
+      context "with invalid attributes" do
+        it "does not create a list item" do
+          set_current_user
+          list = Fabricate(:list)
+          post :create, list_item: Fabricate.attributes_for(:list_item, name: nil), list_id: list.id
+          expect(ListItem.count).to eq(0)
+        end
+        it "sets @list" do
+          set_current_user
+          list = Fabricate(:list)
+          post :create, list_item: Fabricate.attributes_for(:list_item, name: nil), list_id: list.id
+          expect(assigns(:list)).to be_present
+        end
+        it "sets @list_items" do
+          set_current_user
+          list = Fabricate(:list)
+          list_item = Fabricate(:list_item, list: list)
+          post :create, list_item: Fabricate.attributes_for(:list_item, name: nil), list_id: list.id
+          expect(assigns(:list_items)).to match_array([list_item])
+        end
+        it "renders lists show page" do
+          set_current_user
+          list = Fabricate(:list)
+          list_item = Fabricate(:list_item, list: list)
+          post :create, list_item: Fabricate.attributes_for(:list_item, name: nil), list_id: list.id
+          expect(response).to render_template 'lists/show'
+        end
+      end
     end
 
-    context "with unauthenticated users"
+    context "with unauthenticated users" do
+      it "redirects to sign in page" do
+        list = Fabricate(:list)
+        post :create, list_item: Fabricate.attributes_for(:list_item, name: "My List"), list_id: list.id
+        expect(response).to redirect_to sign_in_path
+      end
+    end
   end
 end
