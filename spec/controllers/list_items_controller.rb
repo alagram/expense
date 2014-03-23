c94e5f8cf7ff763fc1f9e48f17208f06818ad1d2
Albert Agram <albert.agram@gmail.com>
2014-03-23 14:34:41 +0000

    ListItems controller happy path specs

--- spec/controllers/list_items_controller.rb
+++ spec/controllers/list_items_controller.rb
@@ -2,7 +2,40 @@ require 'spec_helper'
 
 describe ListItemsController do
   describe "POST create" do
-    context "with authenticated users"
+    context "with authenticated users" do
+
+      context "with valid attributes" do
+
+        it "redirects to list_list_items page" do
+          set_current_user
+          list = Fabricate(:list)
+          post :create, list_item: Fabricate.attributes_for(:list_item, name: "My List"), list_id: list.id
+          expect(response).to redirect_to list_list_items_path
+        end
+        it "create a new list item" do
+          set_current_user
+          list = Fabricate(:list)
+          post :create, list_item: Fabricate.attributes_for(:list_item, name: "My List"), list_id: list.id
+          expect(ListItem.count).to eq(1)
+        end
+        it "create a list item associtaed with current logged in user" do
+          set_current_user
+          alice = current_user
+          list = Fabricate(:list)
+          post :create, list_item: Fabricate.attributes_for(:list_item, name: "My List"), list_id: list.id
+          expect(ListItem.first.user).to eq(alice)
+        end
+        it "dislays success message" do
+          set_current_user
+          list = Fabricate(:list)
+          post :create, list_item: Fabricate.attributes_for(:list_item, name: "My List"), list_id: list.id
+          expect(flash[:success]).to be_present
+        end
+      end
+
+      context "with invalid attributes"
+    end
+
     context "with unauthenticated users"
   end
 end
