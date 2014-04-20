42d2a17b3c9dcd70d093c2a44d26a43bb4dbb4c2
Albert Agram <albert.agram@gmail.com>
2014-04-20 14:26:50 +0000

    Show only logged in users shops

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def grand_total(collection)
     collection.map(&:total).inject(:+)
   end
+
+  def shop_for_user
+    current_user.shops.map { |shop| [shop.name, shop.id] }
+  end
 end
